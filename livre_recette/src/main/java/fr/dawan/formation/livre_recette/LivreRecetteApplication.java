0ccea1caeac16574da964501fc90937bb42f864b
Christine Duarte <christine_dos_santos@hotmail.fr>
2022-12-14 23:54:23 +0100

    creation microservice livre_recette et planification_repas

---
+++ livre_recette/src/main/java/fr/dawan/formation/livre_recette/LivreRecetteApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.formation.livre_recette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivreRecetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivreRecetteApplication.class, args);
+	}
+
+}
---
+++ planification_repas/src/main/java/fr/dawan/planification_repas/PlanificationRepasApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.planification_repas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanificationRepasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanificationRepasApplication.class, args);
+    }
+
+}
