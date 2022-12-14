cfa44a7738fd2430769ac8caa6b465f601b78a29
Christine Duarte <christine_dos_santos@hotmail.fr>
2022-12-14 22:54:04 +0100

    creation microservice client_ui et ajout des differentes vues

---
+++ client_ui/src/test/java/fr/dawan/formation/client_ui/ClientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.formation.client_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
